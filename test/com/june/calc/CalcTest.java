4bd29a9f0ab469e354ecb769b00735a91753536a
June <ucessdori@gmail.com>
2024-11-15 18:14:24 +0900

    테스트 수정

--- test/com/june/calc/CalcTest.java
+++ test/com/june/calc/CalcTest.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.Test;
 class CalcTest {
 
 	@Test
-	void test() {
+	void test()
+	{
+		KCalc.setLogging(true);
 		// 파라메타 세팅을 위한 map 선언
 		HashMap<String, Object> map = new HashMap<>();
-		map.put("A", new BigDecimal(2.0));
-		map.put("B", 5.0);
+		map.put("A", "A001");
+		map.put("B", new BigDecimal(5));
 		// 계산식 세팅
-		String formula = "A * ( B + 11 ) / 3";
+		String formula = "A == 'A001' && B == 5";
 		// 계산실행
 		BigDecimal result = (BigDecimal) KCalc.calculate(map, formula);
 
